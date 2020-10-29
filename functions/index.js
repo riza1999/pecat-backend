d4fae5653a63aa54b879093ccf785b7dba08ea2d
Riza <rizakurniawan48@gmail.com>
2020-10-29 15:22:53 +0700

    function login

--- functions/index.js
+++ functions/index.js
@@ -5,41 +5,64 @@ const db = admin.firestore();
 
 const express = require('express');
 const api = express();
+const bodyParser = require('body-parser');
 
 const cors = require('cors')({origin: true});
 api.use(cors);
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({ extended: true }));
 
-api.get('/cat', (req, res) => {
-   res.send('CAT');
-});
+api.post('/login', async (req,res) => {
+   let {email,password} = req.body;
 
-api.get('/getDataEmployee', (req,res) => {
-   db.collection("Employee").get().then(function(querySnapshot){
-      querySnapshot.forEach(doc => {
-         let hasil = doc.data();
-         let {name,email} = hasil;
+   const snapshot = await db.collection("Employee")
+                              .where('email','==', email)
+                              .where('password','==', password)
+                              .get();
 
-         if(hasil.job_id) {
-            hasil.job_id.get().then(resp => {
-               hasil.blabla = resp.data();
-               let job_name = hasil.blabla.name;
+   if(snapshot.empty){
+      //jika password dan email salah
+      res.json({
+         success: false,
+         info: 'Invalid email or password',
+         employee: {}
+      });
+   }
 
-               return res.json({
+   const uid = snapshot.docs[0].id;
+   snapshot.forEach(doc =>{
+      let user = doc.data();
+      let {name,email} = user;
+
+      if(user.job_id) {
+         user.job_id.get().then(resp => {
+            user.blabla = resp.data();
+            let job_name = user.blabla.name;
+
+            //jika normal
+            res.json({
+               success: true,
+               employee: {
+                  uid: uid,
                   name: name,
                   email: email,
                   job_name: job_name
-               });
-            })
-         }else{
-            return res.json({
+               }
+            });
+         })
+      }else{
+         //jika tidak ada job_id
+         res.json({
+            success: true,
+            employee: {
+               uid: uid,
                name: name,
                email: email
-            });
-         }
-      });
-    }).catch(err => {
-       console.log('Error getting documents', err);
-    });
+            }
+         });
+      }
+   })
 })
+
 
 exports.api = functions.https.onRequest(api);
